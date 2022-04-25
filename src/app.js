f4fde6139014e7ad6bc34bd058a4e1264d45b596
workerKwon <dmdmdm1322@gmail.com>
2022-04-25 23:27:56 +0900

    많이 사용하는 plugin - BannerPlugin : 빌드 정보를 추가해서 잘 배포되었는지 확인하는 용도 - DefinePlugin : 빌드 타임에 결정되는 환경변수를 어플리케이션 단에 주입하기 위해 사용 - HtmlTemplatePlugin : html을 빌드 과정에 넣을 수 있게 해준다. - CleanWebpackPlugin : 빌드 할 때마다 output 폴더(dist)를 삭제 해주는 플러그인 - MiniCssExtractPlugin : 번들된 js 코드에서 css 코드만 따로 뽑아내서 css 파일을 만들어주는 플러그인

--- src/app.js
+++ src/app.js
@@ -5,3 +5,7 @@ import './app.css'
 import math from './math'
 
 console.log(math.sum(1, 2))
+
+console.log(process.env.NODE_ENV)
+console.log(TWO)
+console.log(api.domain)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
-const MyWebpackPlugin = require('./my-webpack-plugin')
+const webpack = require('webpack')
+const childProcess = require('child_process')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development', // 웹팩 실행 모드
@@ -26,15 +30,16 @@ module.exports = {
             {
                 test: /\.css$/, // .css로 끝나는 파일들은 아래의 로더들을 거친다.
                 use: [
-                    'style-loader',
+                    process.env.NODE_ENV === 'production' ?
+                        MiniCssExtractPlugin.loader : 'style-loader',
                     'css-loader'
                 ]
             },
             {
                 test: /\.png$/,
-                loader: 'file-loader',
+                loader: 'url-loader',
                 options: {
-                    publicPath: './dist',
+                    // publicPath: './dist', // HtmlWebpackPlugin으로 인해서 html이 dist 폴더 안으로 들어가게 되어서 필요없어지게 된다.
                     name: '[name].[ext]?[hash]'
                 },
             }
@@ -47,6 +52,35 @@ module.exports = {
      * 번들된 자바스크립트를 난독화 한다거나 특정 텍스트를 추출하는 용도로 사용한다.
      */
     plugins: [
-        new MyWebpackPlugin(),
+        new webpack.BannerPlugin({
+            banner: `
+                Build Date: ${new Date().toLocaleString()}
+                Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+                Author: ${childProcess.execSync('git config user.name')}
+            `
+        }),
+        new webpack.DefinePlugin({
+            TWO: JSON.stringify('1+1'),
+            'api.domain': JSON.stringify('http://dev.api.domain.com')
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            templateParameters: {
+                env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+            },
+            minify: process.env.NODE_ENV === 'production' ? {
+                collapseWhitespace: true, // 빈칸 제거
+                removeComments: true // 주석 제거
+            } : false
+        }),
+        new CleanWebpackPlugin(), // 빌드 할 때마다 dist 폴더를 삭제해주는 플러그인(dist 폴더가 새로 생성될 수 있게 매번 삭제 해준다)
+
+        /**
+         * css 파일이 클 경우에는 js 하나에 번들할 경 로딩 시간이 오래걸리기 때문에 css파일, js 파일로 나누는 것이 좋다.
+         * css 파일을 따로 빼주는 플러그인. 개발환경에서는 굳이 필요하지 않기 때문에 production 모드에서만 동작하도록 하는것이 좋다.
+         */
+        ...(process.env.NODE_ENV === 'production' ?
+            [new MiniCssExtractPlugin({filename: '[name].css'})] : []
+        )
     ]
 }
